60a78c8437ac3600010db67d75f76bb8f2478799
meeksjack95 <meeksjack95@gmail.com>
2022-04-05 03:04:02 -0600

    fixed layouts typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const indexRouter = require('./routes/index')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
+app.set('layout', 'Layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
